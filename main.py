974989b58b57e071d608a72819dc19c5aae62b43
Jacky Chong <jackychh7878@gmail.com>
2024-12-08 12:09:58 +0800

    feat(code for llama):

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import sys
 import boto3
 
 prompt = """
-
     You are a smart assistant so please let me know what is machine learning in the smartest way?
 """
 
-bedrock = boto3.client(service_name="bedrock-runtime")
+bedrock = boto3.client("bedrock-runtime")
 
 payload = {
-
-
+    "prompt": "[INST]" + prompt + "[/INST]",
+    "max_gen_len": 512,
+    "temperature": 0.3,
+    "top_p": 0.9,
 }
 
 body = json.dumps(payload)
-model_id = "meta.llama3-70b-instruct-v1:0"
+model_id = "meta.llama3-8b-instruct-v1:0"
 
-
-response=bedrock.invoke_model(
+response = bedrock.invoke_model(
     body=body,
-    model_id=model_id,
+    modelId=model_id,
     accept="application/json",
-    content_type="application/json"
+    contentType="application/json"
 )
 
 response_body = json.loads(response.get("body").read())
 response_text = response_body["generation"]
+
 print(response_text)
